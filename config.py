8bde34ffcf9a4bf55c422e57c9029457fb20b898
nirav369369 <niravdomadiya1@gmail.com>
2025-06-29 15:54:12 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+# Binance API keys from environment variables
+BINANCE_API_KEY = os.environ.get("BINANCE_API_KEY")
+BINANCE_API_SECRET = os.environ.get("BINANCE_API_SECRET")
+
+# BingX API keys from environment variables
+BINGX_API_KEY = os.environ.get("BINGX_API_KEY")
+BINGX_API_SECRET = os.environ.get("BINGX_API_SECRET")
